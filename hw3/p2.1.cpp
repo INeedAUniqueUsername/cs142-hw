efad495772790f0711386b1f16443bc4cef9dbe7
Alex Chen <achen115@ucr.edu>
2021-02-14 22:37:18 -0800

    p2.1 attempt 1

---
+++ hw3/p2.1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include "get_time.h"
+using namespace std;
+
+#define FOR(VAR, COUNT) for(int VAR = 0; VAR < COUNT; ++VAR)
+
+#define MULTIPLY(i, j, k)					\
+FOR(i, n) {									\
+	FOR(j, n) {								\
+		FOR(k, n) {							\
+			cout << 1;						\
+			c[i][j] += a[i][k] * b[j][j];	\
+		}									\
+	}										\
+}
+
+#define INIT								\
+FOR(i, n) {									\
+	FOR(j, n) {								\
+		a[i][j] = i + j;					\
+		b[i][j] = i + 2 * j;				\
+	}										\
+}
+
+#define TIME(CODE) {						\
+	timer t;								\
+	t.start();								\
+	CODE									\
+	t.stop();								\
+	cout << "CODE time: " << t.get_total() << endl; \
+}
+
+int main() {
+	cout << "a" << endl;
+
+	unsigned n = 1024;
+	int a[n][n];
+	int b[n][n];
+	cout << "a" << endl;
+	int c[n][n];
+	cout << "a" << endl;
+	FOR(aaaa, n) { cout << aaaa << endl; }
+
+	INIT
+		TIME(MULTIPLY(i, j, k))
+	INIT
+	TIME(MULTIPLY(i, k, j))
+	INIT
+	TIME(MULTIPLY(j, i, k))
+	INIT
+	TIME(MULTIPLY(j, k, i))
+	INIT
+	TIME(MULTIPLY(k, i, j))
+	INIT
+	TIME(MULTIPLY(k, j, i))
+}
