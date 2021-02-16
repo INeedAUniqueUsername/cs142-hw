f73cb7e14519064346700c8c7c521f7f39d59ed0
Alex Chen <achen115@ucr.edu>
2021-02-15 20:29:18 -0800

    c

--- hw3/p2.1.cpp
+++ hw3/p2.1.cpp
@@ -38,15 +38,20 @@ int c[n][n];
 int main() {
 	INIT
 		TIME(MULTIPLY(i, j, k), "ijk")
-		cout << c[10][20] << endl;
+		cout << "c[10][20] = " << c[10][20] << endl;
 	INIT
 	TIME(MULTIPLY(i, k, j), "ikj")
+		cout << "c[10][20] = " << c[10][20] << endl;
 	INIT
 	TIME(MULTIPLY(j, i, k), "jik")
+		cout << "c[10][20] = " << c[10][20] << endl;
 	INIT
 	TIME(MULTIPLY(j, k, i), "jki")
+		cout << "c[10][20] = " << c[10][20] << endl;
 	INIT
 	TIME(MULTIPLY(k, i, j), "kij")
+		cout << "c[10][20] = " << c[10][20] << endl;
 	INIT
 	TIME(MULTIPLY(k, j, i), "kji")
+		cout << "c[10][20] = " << c[10][20] << endl;
 }
